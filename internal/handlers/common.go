9b8c42f253eff435e81d203cf98e69e13f15aa78
kksama <kksama77@gmail.com>
2024-08-01 00:57:21 +0300

    changed docker-compose

--- internal/handlers/common.go
+++ internal/handlers/common.go
@@ -30,13 +30,14 @@ func NewService(pool *sql.DB) *Service {
 
 func (s *Service) SendMsgToKafka() {
 	var helpSlice []model.Message
-	for len(s.Messages) > 0 {
+	log.Println("SendMsgToKafka: len(s.Messages) = ", len(s.Messages))
+	for len(s.Messages) != 0 {
 		err := kafka.SendMsg(s.Messages[0].ID, s.Messages[0].Content)
 		if err != nil {
 			log.Printf("error during sendimg msg via kafka : %v\n", err)
 			helpSlice = append(helpSlice, s.Messages[0])
 		}
-
+		log.Println("SendMsgToKafka: msg sent")
 		s.Messages = s.Messages[1:]
 	}
 
